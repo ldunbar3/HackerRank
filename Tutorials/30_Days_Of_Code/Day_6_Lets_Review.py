8b925c993566659ce9828af6fe0f87e21cb14e76
ldunbar3 <ldunbar@mac.com>
2019-09-01 19:06:15 -0400

    Day 6 - Lets Review

--- Tutorials/30_Days_Of_Code/Day_6_Lets_Review.py
+++ Tutorials/30_Days_Of_Code/Day_6_Lets_Review.py
@@ -1,15 +1,15 @@
 # https://www.hackerrank.com/challenges/30-review-loop/problem
 
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
 
-N = int(input())
-for i in range(0, N):
-    string = input()
-    evens = []
-    odds = []
-    for k, v in enumerate(string):
-        if k == 0 or k % 2 == 0:
-            evens.append(v)
-        else:
-            odds.append(v)
-    print(f"{''.join(evens)} {''.join(odds)}")
+    N = int(input())
+    for i in range(0, N):
+        string = input()
+        evens = []
+        odds = []
+        for k, v in enumerate(string):
+            if k == 0 or k % 2 == 0:
+                evens.append(v)
+            else:
+                odds.append(v)
+        print(f"{''.join(evens)} {''.join(odds)}")
