de8e17e3bdbde657e8e045685c02ab0bbb84a123
ldunbar3 <ldunbar@mac.com>
2019-09-13 10:53:52 +0300

    Day 2 Operators

---
+++ Tutorials/30_Days_Of_Code/Day_11_2d_Arrays.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+#!/bin/python3
+
+
+arr = [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 2, 4, 4, 0], [0, 0, 0, 2, 0, 0], [0, 0, 1, 2, 4, 0]]
+
